7d0a4c0ac790d3ee4d979765064f2f0963a81468
febelle250 <febelle250@gmail.com>
2025-04-21 17:31:20 +0200

    Update random_forest.py

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -1,3 +1,9 @@
+import pandas as pd
+
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 X = df1_imputed.drop("Risk", axis=1)
 y = df1_imputed["Risk"]
 
