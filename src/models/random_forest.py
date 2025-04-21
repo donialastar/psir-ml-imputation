242be06b3f5c6f10da4a7d947ceda74c896cf217
febelle250 <febelle250@gmail.com>
2025-04-21 16:59:11 +0200

    Add files via upload

---
+++ src/models/random_forest.py
@@ -0,0 +1,7 @@
+X = df1_imputed.drop("Risk", axis=1)
+y = df1_imputed["Risk"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
